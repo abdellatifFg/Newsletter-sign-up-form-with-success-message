862e7dc8740939822de489cfa4a626462a4dffaf
abdellatif feghouli <ka_feghouli@esi.dz>
2023-08-01 01:51:23 +0200

    Responsive Design for Mobile

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
 const form = document.querySelector("form");
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-    document.getElementsByClassName("container")
 
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const container = document.querySelector(".container");
+  const successMsgContainer = document.querySelector(".success-msg-container");
+  container.classList.add("hided");
+  successMsgContainer.classList.remove("hided");
+  console.log(email);
+});
