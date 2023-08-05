e3aa3d471fe84644c65d43457e08c73efa467a53
abdellatif feghouli <ka_feghouli@esi.dz>
2023-08-06 00:20:03 +0200

    switching between sections

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const form = document.querySelector("form");
+const sign_up = document.querySelector(".container");
+const success = document.querySelector(".success-msg-container");
+const submittedEmail = document.querySelector(".submitted-email");
+const dismiss = document.querySelector(".dismiss");
+
+function swithSections() {
+  sign_up.classList.toggle("hided");
+  success.classList.toggle("hided");
+}
+
+function updateSucessMsg(email) {
+  submittedEmail.textContent = email;
+}
+
+function isValidEmail(val) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return val.match(mailformat);
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const container = document.querySelector(".container");
-  const successMsgContainer = document.querySelector(".success-msg-container");
-  container.classList.add("hided");
-  successMsgContainer.classList.remove("hided");
-  console.log(email);
+  const email = form.querySelector("#email").value.toString();
+  if (isValidEmail(email)) {
+    updateSucessMsg(email);
+    swithSections();
+  }
+});
+
+dismiss.addEventListener("click", (e) => {
+  updateSucessMsg("");
+  swithSections();
 });
